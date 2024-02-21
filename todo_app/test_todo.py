014a968e146c9b6bffdcea20f945046e7f14513e
Venelin Pavlov <106750644+VPavlov16@users.noreply.github.com>
2024-02-21 10:44:54 +0200

    updates

--- todo_app/test_todo.py
+++ todo_app/test_todo.py
@@ -1,19 +1,28 @@
 import pytest
 from todo import TodoList
 
+# test za dobavqne na zadacha
 def test_add_task():
+    print()
+    print("Тест за добавяне на задача")
     todo_list = TodoList()
-    todo_list.add_task("Do laundry")
-    assert todo_list.tasks == ["Do laundry"]
+    todo_list.add_task("Сгъни си прането")
+    assert todo_list.tasks == ["Сгъни си прането"]
 
+#test za mahane na zadacha
 def test_remove_task():
+    print()
+    print("Тест за премахване на задача")
     todo_list = TodoList()
-    todo_list.add_task("Do laundry")
-    todo_list.remove_task("Do laundry")
+    todo_list.add_task("Сгъни си прането")
+    todo_list.remove_task("Сгъни си прането")
     assert todo_list.tasks == []
 
+#test za markirane na zadacha kato napravena
 def test_mark_task_completed():
+    print()
+    print("Тест за успешно изпълнена задача")
     todo_list = TodoList()
-    todo_list.add_task("Do laundry")
-    todo_list.mark_completed("Do laundry")
-    assert todo_list.completed_tasks == ["Do laundry"]
+    todo_list.add_task("Сгъни си прането")
+    todo_list.mark_completed("Сгъни си прането")
+    assert todo_list.completed_tasks == ["Сгъни си прането"]
--- todo_app/todo.py
+++ todo_app/todo.py
@@ -20,24 +20,3 @@ class TodoList:
             self.completed_tasks.append(task)
 
 
-# test_todo.py
-
-import pytest
-from todo import TodoList
-
-def test_add_task():
-    todo_list = TodoList()
-    todo_list.add_task("Do laundry")
-    assert todo_list.tasks == ["Do laundry"]
-
-def test_remove_task():
-    todo_list = TodoList()
-    todo_list.add_task("Do laundry")
-    todo_list.remove_task("Do laundry")
-    assert todo_list.tasks == []
-
-def test_mark_task_completed():
-    todo_list = TodoList()
-    todo_list.add_task("Do laundry")
-    todo_list.mark_completed("Do laundry")
-    assert todo_list.completed_tasks == ["Do laundry"]
